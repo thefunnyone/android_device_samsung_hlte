de47cb24d668f0e3874d54812dec8885a2a6a9d9
thefunnyone <thefunnyone@users.noreply.github.com>
2015-07-05 16:44:18 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+$(call inherit-product, device/samsung/hlte/full_ks01lte.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
