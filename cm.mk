36630c3841d01bec623e0eaf3cc6cfc56b305cd2
thefunnyone <thefunnyone@users.noreply.github.com>
2015-07-05 16:43:01 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -6,5 +6,5 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_DEVICE := hlte
-PRODUCT_NAME := cm_hlte
+PRODUCT_DEVICE := ks01lte
+PRODUCT_NAME := cm_ks01lte
